b3052d485832362a70510e02648d8fede9c214e6
lcano@unizar.es <lcano@unizar.es>
2024-02-28 17:10:48 +0100

    changed how info is fed into the node in the launch file

--- gallery_detection_ros/nodes/gallery_detection_model_node.py
+++ gallery_detection_ros/nodes/gallery_detection_model_node.py
@@ -1,4 +1,4 @@
-#!/bin/python3
+#!/home/lorenzo/miniconda3/envs/dinf/bin/python3.8
 import pathlib
 import torch
 from cv_bridge import CvBridge
@@ -16,36 +16,24 @@ class NetworkNode:
         )
         self.init_network()
         self._cv_bridge = CvBridge()
-        self.image_subscriber = rospy.Subscriber(
-            "/lidar_image_norm", sensor_msg.Image, self.image_callback
-        )
+        image_topic = rospy.get_param("~image_topic")
+        self.image_subscriber = rospy.Subscriber(image_topic, sensor_msg.Image, self.image_callback)
         self.detection_publisher = rospy.Publisher(
             "/gallery_detection_vector", std_msg.Float32MultiArray, queue_size=10
         )
 
     def init_network(self):
-        file_path = rospy.get_param(
-            "~nn_path",
-            default="/home/lorenzo/models/gallery_detection/procedural_datasets/dataset_03/gallery_detector_v3-_r10_lr002_4.torch",
-        )
-        print(file_path)
+        file_path = rospy.get_param("~nn_path")
         file_name = pathlib.Path(file_path).name
-        nn_type = file_name.split("-")[0]
+        nn_type = file_name.split(".")[0]
         module = importlib.import_module("gallery_detection_models.models")
         self.model = getattr(module, nn_type)()
-        rospy.set_param("/ptcl_to_img/image_width", self.model.input_width)
-        rospy.set_param("/ptcl_to_img/image_height", self.model.input_height)
-        self.model.load_state_dict(
-            torch.load(file_path, map_location=torch.device("cpu"))
-        )
+        self.model.load_state_dict(torch.load(file_path, map_location=torch.device("cpu")))
         self.model.eval()
 
     def image_callback(self, msg: sensor_msg.Image):
-        depth_image = np.reshape(
-            np.frombuffer(msg.data, dtype=np.float32), (msg.height, msg.width)
-        )
+        depth_image = np.reshape(np.frombuffer(msg.data, dtype=np.float32), (msg.height, msg.width))
         depth_image_tensor = torch.tensor(depth_image).float().to(torch.device("cpu"))
-        depth_image_tensor /= torch.max(depth_image_tensor)
         depth_image_tensor = torch.reshape(depth_image_tensor, [1, 1, 16, -1])
         data = self.model(depth_image_tensor)
         data = data.cpu().detach().numpy()
