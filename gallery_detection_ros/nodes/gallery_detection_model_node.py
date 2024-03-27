d4d0473b721e3db346909bdaef7145e4c38fae5c
lcano@unizar.es <lcano@unizar.es>
2024-03-27 18:09:36 +0100

    nuse

--- gallery_detection_ros/nodes/gallery_detection_model_node.py
+++ gallery_detection_ros/nodes/gallery_detection_model_node.py
@@ -8,6 +8,20 @@ import rospy
 import importlib
 import numpy as np
 
+from gallery_detection_models.models import GalleryDetectorV3
+
+# model = GalleryDetectorV3(debug=True)
+# model.load_state_dict(
+#    torch.load(
+#        "/media/lorenzo/SAM500/models/gallery-detection/GalleryDetectorV3.v2.torch",
+#        map_location=torch.device("cuda"),
+#    ),
+# )
+# model.eval()
+# model.to("cuda")
+# model(torch.rand((1, 1, 16, 720)).to("cuda"))
+# exit()
+
 
 class NetworkNode:
     def __init__(self):
@@ -28,12 +42,15 @@ class NetworkNode:
         nn_type = file_name.split(".")[0]
         module = importlib.import_module("gallery_detection_models.models")
         self.model = getattr(module, nn_type)()
-        self.model.load_state_dict(torch.load(file_path, map_location=torch.device("cpu")))
+        self.model.load_state_dict(torch.load(file_path))  # , map_location=torch.device("cpu")))
         self.model.eval()
 
     def image_callback(self, msg: sensor_msg.Image):
-        depth_image = np.reshape(np.frombuffer(msg.data, dtype=np.float32), (msg.height, msg.width))
-        depth_image_tensor = torch.tensor(depth_image).float().to(torch.device("cpu"))
+        depth_image_raw = np.reshape(
+            np.frombuffer(msg.data, dtype=np.float32), (msg.height, msg.width)
+        )
+        depth_image_norm = depth_image_raw / np.max(depth_image_raw)
+        depth_image_tensor = torch.tensor(depth_image_norm).float().to(torch.device("cpu"))
         depth_image_tensor = torch.reshape(depth_image_tensor, [1, 1, 16, -1])
         data = self.model(depth_image_tensor)
         data = data.cpu().detach().numpy()
